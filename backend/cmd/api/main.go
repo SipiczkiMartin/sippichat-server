1cd05febd4c5d3e7514bd114d3e7d16ce3d4b821
Martin Sipiczki <sipiczki45@gmail.com>
2026-07-25 10:23:54 +0200

    initial backend project

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	log.Println("Server listening on :8080")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
